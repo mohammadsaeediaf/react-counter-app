55449ac5a4aa9847e9d84224c3a5bfdddaa52a87
VanGuardf <alisaadati18000@gmail.com>
2024-01-25 21:40:48 +0330

    Add App componnent

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function App() {
+  const [count, setcount] = useState(0);
+  const [step, setstep] = useState(1);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+  return (
+    <div className="container">
+      <div className="step">
+        <button onClick={() => setstep(step + 1)}>+</button>
+        <span>step : {step}</span>
+        <button onClick={() => setstep(step - 1)}>-</button>
+      </div>
+
+      <div className="count">
+        <button onClick={() => setcount(count + step)}>+</button>
+        <span>count : {count}</span>
+        <button onClick={() => setcount(count - step)}>-</button>
+      </div>
+
+      <div className="output">
+        <h3>
+          {count === 0
+            ? "Today is: "
+            : `${
+                count > 0
+                  ? `${count} days from today is`
+                  : `${Math.abs(count)} days ago was `
+              } `}{" "}
+          {date.toDateString()}
+        </h3>
+      </div>
+    </div>
+  );
+}
